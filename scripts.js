7b6bf7febe82f3df7a344e6a0202e3b7e642f642
powder-rose <glittervodka@yandex.ru>
2022-11-24 19:37:38 +0300

    second commit

--- scripts.js
+++ scripts.js
@@ -1,16 +1,12 @@
-
- 
- 
-
-   let checkForm = () => {
+let username = document.getElementById('name').value
+let lastName = document.getElementById('lastName').value 
+let login = document.getElementById('login').value
+let password = document.getElementById('password').value
+let passwordRepeat = document.getElementById('passwordRepeat').value
+let number = document.getElementById('phonenumber').value
+     
     
-    let username = document.getElementById('name').value
-    let lastName = document.getElementById('lastName').value 
-    let login = document.getElementById('login').value
-    let password = document.getElementById('password').value
-    let passwordRepeat = document.getElementById('passwordRepeat').value
-    let number = document.getElementById('phonenumber').value 
-    let result = 'Добро Пожаловать'
+     let checkForm = () => {
 
     if (username == "") {
       alert('Пожалуйста, введите имя') 
@@ -18,7 +14,7 @@
     } else if (lastName == "") {
        alert('Пожалуйста, введите фамилию')
        return false
-    } else if (login == '') {
+    } else if (login == "") {
       alert('Пожалуйста, придумайте логин')
       return false
     } else if (password == "") {
@@ -32,23 +28,14 @@
         return false
     } else if (number == "") {
        alert('Пожалуйста, введите номер')
-       return false
-       } else if (number.length != 10 || isInteger(number) == false) {
-      alert('Пожалуйста, введите корректный номер')
-      return false } 
+        return false
+    } else if (number.length != 10 || isInteger(number) == false) {
+       alert('Пожалуйста, введите корректный номер')
+        return false } 
       
       return true
      
-      
-      
     }
-
-
-
-
-   
-
-
   
    let btn = document.getElementById('submit')
    btn.onclick = checkForm
